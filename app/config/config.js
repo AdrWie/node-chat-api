8a871740e7e4bbcb0815e76ef43f4a4cf2346412
Adrian <adde15w@hotmail.com>
2018-05-14 12:39:48 +0200

    added production env variables in config.json

--- app/config/config.js
+++ app/config/config.js
@@ -2,7 +2,7 @@
 var env = process.env.NODE_ENV || 'development';
 
 // Grab settings for local development
-if(env === 'development') {
+if(env === 'development' || env === 'production') {
   var config = require('./config.json');
   var envConfig = config[env];
 
